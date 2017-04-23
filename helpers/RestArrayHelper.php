5b3f75cb6db912225b976569978dd8af577f8cf5
ravil <ravil.shamenov@altarix.ru>
2017-04-23 19:25:26 +0400

    поправил доку

--- helpers/RestArrayHelper.php
+++ helpers/RestArrayHelper.php
@@ -16,39 +16,10 @@ use yii\helpers\BaseArrayHelper;
 class RestArrayHelper extends BaseArrayHelper
 {
     /**
-     * Converts an object or an array of objects into an array.
-     * @param object|array|string $object the object to be converted into an array
-     * @param array $properties a mapping from object class names to the properties that need to put into the resulting arrays.
-     * The properties specified for each class is an array of the following format:
+     * Преобразует модели в массивы с учетом RestSerializable интерфейса
      *
-     * ```php
-     * [
-     *     'app\models\Post' => [
-     *         'id',
-     *         'title',
-     *         // the key name in array result => property name
-     *         'createTime' => 'created_at',
-     *         // the key name in array result => anonymous function
-     *         'length' => function ($post) {
-     *             return strlen($post->content);
-     *         },
-     *     ],
-     * ]
-     * ```
-     *
-     * The result of `ArrayHelper::toArray($post, $properties)` could be like the following:
-     *
-     * ```php
-     * [
-     *     'id' => 123,
-     *     'title' => 'test',
-     *     'createTime' => '2013-01-01 12:00AM',
-     *     'length' => 301,
-     * ]
-     * ```
-     *
-     * @param bool $recursive whether to recursively converts properties which are objects into arrays.
-     * @return array the array representation of the object
+     * @inheritdoc
+     * @param array $properties массив конфигов полей для каждого типа моделек
      */
     public static function toArray($object, $properties = [], $recursive = true)
     {
