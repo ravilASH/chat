05c3d99574d8e1c12879b4303a68c645c111aef5
ravil.shamenov@altarix.ru <ravil.shamenov@altarix.ru>
2017-04-25 09:34:44 +0400

    node

---
+++ node/chat.js
@@ -0,0 +1,23 @@
+/**
+ * Created by ravil on 18.04.17.
+ */
+
+var clients = {};
+
+exports.subscribe = function (req, res, clientId) {
+    if (clients[clientId] == undefined ){
+        clients[clientId] = [];
+    }
+    clients[clientId].push(res);
+    res.on('close', function () {
+        clients[clientId].splice(clients[clientId].indexOf(res), 1);
+    });
+};
+
+exports.publish = function (message, clientIds) {
+    clientIds.forEach(function (clientId) {
+        clients[clientId].forEach(function (res) {
+            res.end(message);
+        });
+    });
+};
---
+++ node/server.js
@@ -0,0 +1,36 @@
+/**
+ * Created by ravil on 18.04.17.
+ */
+
+var http = require('http');
+var chat = require('./chat');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    urlParsed = url.parse(req.url, true);
+
+    switch ( urlParsed.pathname ) {
+        case '/subscribe':
+            chat.subscribe(req , res, urlParsed.query.id);
+            break;
+        case '/publish' :
+            var body = '';
+            req
+                .on('readable', function () {
+                    var tmp = req.read();
+                    body += (tmp) ? tmp : '';
+                })
+                .on('end', function () {
+                    // здесь надо переделать способ передаци айдишников на более универсальный
+                    // todo сделать try - catch и логи
+                    bodyParsed = JSON.parse(body);
+                    clientIds = bodyParsed.chat.userIds;
+                    chat.publish(body, clientIds);
+                    res.end('ok');
+                });
+            break;
+        default :
+            res.statusCode = 404;
+            res.end('Not Found');
+    }
+}).listen(9090);
